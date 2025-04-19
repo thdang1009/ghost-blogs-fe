c0035b1f9d7dbb2d9c570e0e84adaecac85b6c7b
Ghost <thdang1009@gmail.com>
2025-04-19 12:08:31 +0700

    ci: fix ci

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+import { environment } from '@environments/environment';
+
+if (environment.production) {
+  console.log = () => {};
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
