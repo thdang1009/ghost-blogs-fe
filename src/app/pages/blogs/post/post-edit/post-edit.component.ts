ff5b8718665b4480e76b05795994186d67aa9e4a
Dang Trinh <thdang1009@gmail.com>
2025-05-17 12:17:57 +0700

    test verify

--- src/app/pages/blogs/post/post-edit/post-edit.component.ts
+++ src/app/pages/blogs/post/post-edit/post-edit.component.ts
@@ -38,7 +38,6 @@ export class PostEditComponent implements OnInit, OnDestroy {
   elem: any;
   POST_TYPE = POST_TYPE;
   listPostType = [
-    // POST_TYPE.GHOST_EDITOR,
     POST_TYPE.MARKDOWN
   ];
   listPermisson = [
