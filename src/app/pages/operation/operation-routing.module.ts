8ea10acf8b49ec1430d5cf48bb0b66b1c986fe8e
Dang Trinh <thdang1009@gmail.com>
2025-08-03 10:38:25 +0700

    fix: fix bug route

--- src/app/pages/operation/operation-routing.module.ts
+++ src/app/pages/operation/operation-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { GrandAdminGuard } from '@guards/grand-admin.guard';
 import { AdminGuard } from '@guards/admin.guard';
 import { TodoLabelListComponent } from './todo-label/todo-label-list/todo-label-list.component';
+import { AddTodoLabelComponent } from './todo-label/add-todo-label/add-todo-label.component';
 import { CouponSettingsComponent } from './coupon-settings/coupon-settings.component';
 import { MoodListComponent } from './mood/mood-list.component';
 
 const routes: Routes = [
   { path: 'todo-label-list', title: `Todo Label`, component: TodoLabelListComponent, canActivate: [AdminGuard] },
+  { path: 'todo-label', title: `Add/Edit Todo Label`, component: AddTodoLabelComponent, canActivate: [AdminGuard] },
   { path: 'coupon-settings', title: `Coupon Settings`, component: CouponSettingsComponent, canActivate: [GrandAdminGuard] },
   { path: 'mood', title: `Mood Types`, component: MoodListComponent, canActivate: [AdminGuard] }
 ];
