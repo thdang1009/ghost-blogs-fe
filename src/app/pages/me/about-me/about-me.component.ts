3aaac180033ea98f65848f0c41d4589e5cccf9f4
Ghost <thdang1009@gmail.com>
2025-10-10 18:16:46 +0700

    feat: add tarotfree to about me

--- src/app/pages/me/about-me/about-me.component.ts
+++ src/app/pages/me/about-me/about-me.component.ts
@@ -1,19 +1,31 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, NgForm, Validators } from '@angular/forms';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { Meta } from '@angular/platform-browser';
 import { GuestMessageService, AlertService } from '@services/_index';
 import { addStructuredData } from '@shared/common';
 
 export interface PortfolioData {
-  src: string,
-  title: string,
-  allowFullscreen: boolean
+  src: string;
+  title: string;
+  allowFullscreen: boolean;
 }
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements OnInit, AfterViewInit {
   @ViewChild('aboutMe') aboutMeElement: ElementRef | undefined;
@@ -27,16 +39,30 @@ export class AboutMeComponent implements OnInit, AfterViewInit {
   yearOlds: number | undefined;
   currentActive = 0;
   debounceID = undefined;
-  listPortfolio: Array<PortfolioData> = [{
-    src: 'https://dangtrinh.site/home',
-    title: 'Personal\'s blogs',
-    allowFullscreen: true
-  }, {
-    src: 'https://mc.zalopay.vn/homepage/index.html',
-    title: 'Merchant tool',
-    allowFullscreen: true
-  }];
-  arrString = ['I\'m Ghost - A Fullstack Dev', 'I\'m Ghost - A Javascript Lover', 'I\'m Ghost - A Minimalist', 'I\'m Ghost - A Book Reviewer', 'I\'m Ghost - A Blogger'];
+  listPortfolio: Array<PortfolioData> = [
+    {
+      src: 'https://dangtrinh.site/home',
+      title: "Personal's blogs",
+      allowFullscreen: true,
+    },
+    {
+      src: 'https://mc.zalopay.vn/homepage/index.html',
+      title: 'Merchant tool',
+      allowFullscreen: true,
+    },
+    {
+      src: 'https://tarotfree.netlify.app/',
+      title: 'Tarot free',
+      allowFullscreen: true,
+    },
+  ];
+  arrString = [
+    "I'm Ghost - A Fullstack Dev",
+    "I'm Ghost - A Javascript Lover",
+    "I'm Ghost - A Minimalist",
+    "I'm Ghost - A Book Reviewer",
+    "I'm Ghost - A Blogger",
+  ];
 
   indexInterval = 0;
   constructor(
@@ -49,16 +75,31 @@ export class AboutMeComponent implements OnInit, AfterViewInit {
     addStructuredData(this.document);
 
     this.meta.updateTag({ itemprop: 'name', content: 'About Dang Trinh' });
-    this.meta.updateTag({ itemprop: 'description', content: 'Detail about Dang Trinh' });
+    this.meta.updateTag({
+      itemprop: 'description',
+      content: 'Detail about Dang Trinh',
+    });
     this.meta.updateTag({ name: 'twitter:card', content: 'summary' });
     this.meta.updateTag({ name: 'twitter:title', content: 'About Dang Trinh' });
-    this.meta.updateTag({ name: 'twitter:description', content: 'Detail about Dang Trinh' });
+    this.meta.updateTag({
+      name: 'twitter:description',
+      content: 'Detail about Dang Trinh',
+    });
     this.meta.updateTag({ name: 'twitter:creator', content: 'Dang Trinh' });
-    this.meta.updateTag({ name: 'twitter:image', content: 'https://dangtrinh.site/assets/img/ghost.png' });
+    this.meta.updateTag({
+      name: 'twitter:image',
+      content: 'https://dangtrinh.site/assets/img/ghost.png',
+    });
     this.meta.updateTag({ property: 'og:title', content: 'About Dang Trinh' });
-    this.meta.updateTag({ property: 'og:description', content: 'Detail about Dang Trinh' });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: 'Detail about Dang Trinh',
+    });
     this.meta.updateTag({ property: 'og:creator', content: 'Dang Trinh' });
-    this.meta.updateTag({ property: 'og:image', content: 'https://dangtrinh.site/assets/img/ghost.png' });
+    this.meta.updateTag({
+      property: 'og:image',
+      content: 'https://dangtrinh.site/assets/img/ghost.png',
+    });
   }
 
   ngOnInit(): void {
@@ -68,7 +109,6 @@ export class AboutMeComponent implements OnInit, AfterViewInit {
       email: [null, Validators.required],
       subject: [null, Validators.required],
       message: [null, Validators.required],
-
     });
   }
   ngAfterViewInit(): void {
@@ -76,21 +116,23 @@ export class AboutMeComponent implements OnInit, AfterViewInit {
       0,
       this.aboutMeElement?.nativeElement.offsetTop,
       this.portfolioElement?.nativeElement.offsetTop,
-      this.contactElement?.nativeElement.offsetTop
+      this.contactElement?.nativeElement.offsetTop,
     ];
   }
   sendMessageToMe(form: NgForm) {
     this.isRunning = true;
-    this.guestMessage.sendGuestMessage(form)
-      .subscribe(res => {
+    this.guestMessage.sendGuestMessage(form).subscribe(
+      res => {
         this.isRunning = false;
         if (res && res.id) {
           this.alertService.showNoti('Send success!', 'success');
         }
-      }, (err) => {
+      },
+      err => {
         this.isRunning = false;
         this.alertService.showNoti('Send Fail! ' + err.error, 'danger');
-      });
+      }
+    );
     // call api save guest message
   }
   scrollTo(s: string) {
@@ -103,9 +145,7 @@ export class AboutMeComponent implements OnInit, AfterViewInit {
   hireMe() {
     // TODO not implemented yet
   }
-  onMessageChange() {
-
-  }
+  onMessageChange() {}
 
   checkOffsetTop(event: any) {
     const val = this.aboutMeContent?.nativeElement.scrollTop;
